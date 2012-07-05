8720f23d577496925ddb7dbce1f8be273c72e761
tatico <tatico@gmail.com>
2012-07-05 21:26:08 +0200

    GPL headed added

--- dog.cpp
+++ dog.cpp
@@ -4,6 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+
+/*
+F Varas July 2012
+
+Dog is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Dog is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Dog.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 using namespace std; 
 /*
 
