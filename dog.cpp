07687b403dec7ea20b7d5508197bb10dcb242b63
tatico <tatico@gmail.com>
2012-07-05 21:17:06 +0200

    filename and content from argument

--- dog.cpp
+++ dog.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream> 
 #include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
 using namespace std; 
 /*
 
@@ -10,41 +13,53 @@ the main goal is to provide filename and filecontent from the command line
 int exitcode;
 void exit (int exitcode);
 
-int main (int argc, char* argv[]) 
+int main (int argc, const char* argv[]) 
 { 
-  if (argc <= 1)
+  if (argc <= 2)
     {
     cout << "Usage: " << argv[0] << " string destination \n\n";
     cout << "all extra arguments will be ignored\n";
     //void exit (int exitcode);    
-    exitcode=0;
+    exitcode=4;
     exit(exitcode);
     }
-  char filename1[] = "./text.text";
-  char filecontent[] ="adding new line to file\n";
+  else if (argc > 2)
+    {
+    std::string argv1 = argv[1]; 
+    std::string argv2 = argv[2];
+    //int content = atoi(argv[1]);
+    string content = argv[1];     //fuck it works!!!
+    //string filename = argv[2];
+    /*
+    two next vars are useless, i'll keep them as a reminder by now.
+    */
+    char filename[] = "./text.text";
+    char filecontent[] = "new line added\n";
+    
+    ifstream inFile; 
+    inFile.open(argv[2]);
 
-  ifstream inFile; 
-  inFile.open (filename1);
-  //inFile.open ("text.text");
+    if (inFile.fail()) 
+      { 
+      cout << "Could Not Open file: " << filename << "\n"; 
+      cout << "Creating new file: " << filename << "\n";
+      ofstream outFile;
+      outFile.open(argv[2], ios::app);
+      outFile << "first line \n" << filecontent;
+      outFile.close();
+      } 
 
-  if (inFile.fail()) 
-    { 
-    cout << "Could Not Open file: " << filename1 << "\n"; 
-    cout << "Creating new file: " << filename1 << "\n";
-    ofstream outFile;
-    outFile.open(filename1, ios::app);
-    //outFile << "adding new line to file \n";
-    outFile << "first line \n" << filecontent;
-    outFile.close();
-    } 
-
-  else 
-    { 
-    cout << "File: " << filename1 << " exists and can be opened.\n"; 
-    ofstream outFile;
-    outFile.open(filename1, ios::app);
-    outFile << filecontent;
-    outFile.close();
+    else 
+      { 
+      cout << "File: " << filename << " exists and can be opened.\n"; 
+      ofstream outFile;
+      outFile.open(argv[2], ios::app);
+/*
+add test (if int, or so) if necessary
+*/
+      outFile << content << "\n";
+      outFile.close();
+      }
     } 
   return 0;
 }
